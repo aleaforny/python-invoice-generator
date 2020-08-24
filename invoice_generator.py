b40587b53d2e0e6658c9991a8db2ebfbd533c388
Baptiste V <43846628+aleaforny@users.noreply.github.com>
2020-08-24 23:43:40 +0200

    Update invoice_generator.py
    - Added set_template_text method to handle template customization
    - Fixed bug with self.due_date.strftime

--- invoice_generator.py
+++ invoice_generator.py
@@ -13,6 +13,30 @@ class InvoiceGenerator:
     DATE_FORMAT = "%d %b %Y"
     LOCALE = "fr_FR"
     TIMEZONE = "Europe/Paris"
+    # Below are the default template parameters that can be changed (see https://github.com/Invoiced/invoice-generator-api/)
+    TEMPLATE_PARAMETERS = [
+        "header",
+        "to_title",
+        "ship_to_title",
+        "invoice_number_title",
+        "date_title",
+        "payment_terms_title",
+        "due_date_title",
+        "purchase_order_title",
+        "quantity_header",
+        "item_header",
+        "unit_cost_header",
+        "amount_header",
+        "subtotal_title",
+        "discounts_title",
+        "tax_title",
+        "shipping_title",
+        "total_title",
+        "amount_paid_title",
+        "balance_title",
+        "terms_title",
+        "notes_title",
+    ]
 
     def __init__(self, sender, to,
                  logo=None,
@@ -48,6 +72,7 @@ class InvoiceGenerator:
         self.amount_paid = amount_paid
         self.notes = notes
         self.terms = terms
+        self.template = {}
 
     def _to_json(self):
         """
@@ -55,18 +80,22 @@ class InvoiceGenerator:
         Please note we need also to replace the key sender to from, as per expected in the API but incompatible with from keyword inherent to Python
         We are formatting here the correct dates
         We are also resetting the two list of Objects items and custom_fields so that it can be JSON serializable
+        Finally, we are handling template customization with its dict
         """
         locale.setlocale(locale.LC_ALL, InvoiceGenerator.LOCALE)
         object_dict = self.__dict__
         object_dict['from'] = object_dict.get('sender')
         object_dict['date'] = self.date.strftime(InvoiceGenerator.DATE_FORMAT)
         if object_dict['due_date'] is not None:
-            object_dict['due_date'] = self.date.strftime(InvoiceGenerator.DATE_FORMAT)
+            object_dict['due_date'] = self.due_date.strftime(InvoiceGenerator.DATE_FORMAT)
         object_dict.pop('sender')
         for index, item in enumerate(object_dict['items']):
             object_dict['items'][index] = item.__dict__
         for index, custom_field in enumerate(object_dict['custom_fields']):
             object_dict['custom_fields'][index] = custom_field.__dict__
+        for template_parameter, value in self.template.items():
+            object_dict[template_parameter] = value
+        object_dict.pop('template')
         return json.dumps(object_dict)
 
     def add_custom_field(self, name=None, value=None):
@@ -91,6 +120,13 @@ class InvoiceGenerator:
         response = requests.post(InvoiceGenerator.URL, json=json.loads(json_string), stream=True, headers={'Accept-Language': InvoiceGenerator.LOCALE})
         if response.status_code == 200:
             open(file_path, 'wb').write(response.content)
+
+    def set_template_text(self, template_parameter, value):
+        """ If you want to change a default value for customising your invoice template, call this method """
+        if template_parameter in InvoiceGenerator.TEMPLATE_PARAMETERS:
+            self.template[template_parameter] = value
+        else:
+            raise ValueError("The parameter {} is not a valid template parameter. See docs.".format(template_parameter))
 
     def toggle_subtotal(self, tax="%", discounts=False, shipping=False):
         """ Toggle lines of subtotal """
@@ -119,4 +155,3 @@ class CustomField:
         """ Object constructor """
         self.name = name
         self.quantity = value
-
